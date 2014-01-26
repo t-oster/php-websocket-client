27fdf3f386445575978bdd7bf6ad87a561f88f56
$imon <contact@emersion.fr>
2014-01-26 19:30:06 +0100

    Update WebSocketClientInterface.php

--- src/WebSocketClient/WebSocketClientInterface.php
+++ src/WebSocketClient/WebSocketClientInterface.php
@@ -9,13 +9,7 @@ interface WebSocketClientInterface
     /**
      * @param array $data
      */
-    function onWelcome(array $data);
-
-    /**
-     * @param string $topic
-     * @param string $event
-     */
-    public function onEvent($topic, $event);
+    public function onMessage($data);
 
     /**
      * @param WebSocketClient $client
